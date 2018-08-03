8c349bf83300c7b5d70766887fb5b49ce77af66d
raulmejia <r8a8m8p8@gmail.com>
2018-08-03 09:53:40 -0500

    Segunda version del paquete el cual incluye la funcion getAttr

--- R/regulonDB.R
+++ R/regulonDB.R
@@ -1,32 +1,69 @@
 #' @title regulonDB
 #'
-#' @description
-#' @param
+#' @description Management of SQLite Databases and Query of Fields tools
+#' Depends of \RSQLite, \DBI, \dplyr, \dbplyr
+#' @param database RSQLite object imported previously from an SQLite database file to R
+#' @param mart Dataset table into the database, use the syntax "dataset"
+#' @param filters A character vector with filters with the restrictions of the query restriction of the query
+#' @param value A character vector with the corresponding values for filters
+#' @param cond Conditional when more than one filter is used "and", "or","not
 #' @examples
-#' @export
-#'
+#' # import the SQLite database
+#' ```
+#' path_sqlite3 <- file.choose()
+#' regulon <- DBI::dbConnect(RSQLite::SQLite(), path_sqlite3)
+#' listDatasets(regulon)
+#' listAttributes(regulon, "TF")
+#' getAttr(
+#' database = regulon,
+#' mart = "GENE",
+#' filters = c("operon_id", "operon_name"),
+#' value = c("ECK125235983","cmoM-mukFEB" ),
+#' cond = "and")
+#' GetRegulatedGenesByTF(regulon, "Ara")
+#'  dbDisconnect(regulon)
+#'  ```
 
+#Install packages if needed
 
-listDatasets <- function(x){
-  dbListTables(x)
+if (!require("RSQLite")) {
+  install.packages("RSQLite", ask =FALSE)
+  library(RSQLite)
+}
+if (!require("dplyr")) {
+  install.packages("dplyr", ask =FALSE)
+  library(dplyr)
+}
+if (!require("dbplyr")) {
+  install.packages("dbplyr", ask =FALSE)
+  library(dbplyr)
+}
+if (!require("DBI")) {
+  install.packages("DBI", ask =FALSE)
+  library(DBI)
 }
 
 #' @export
-listAttributes <- function(database, dataset) {
+listDatasets <- function(database){
+  dbListTables(database)
+}
+
+#' @export
+listAttributes <- function(database, mart) {
   tryCatch(
     {
-      regulonAttributes = DBI::dbListFields(database, dataset)
+      regulonAttributes = DBI::dbListFields(database, mart)
       regulonAttributes
     },
     error=function(cond) {
-      message("Error: Database or dataset does not exist")
+      message("Error: Database or mart does not exist")
     }
   )
 }
 
 #' @export
-GetRegulatedGenesByTF <- function(x,tf){
-  genes_regulon <- tbl(x, "GENE")
+GetRegulatedGenesByTF <- function(database,tf){
+  genes_regulon <- tbl(database, "GENE")
   tf_table <- genes_regulon %>%
     select(name, gene_tf)
   result <- tf_table %>% filter( dplyr::sql( paste0("gene_tf LIKE '%",tf,"%' ") )) %>% select("name")
@@ -35,6 +72,19 @@ GetRegulatedGenesByTF <- function(x,tf){
 
 }
 
-
+#' @export
+getAttr <- function(database, mart, filters, value, cond ){
+  query_cmd <- "SELECT * FROM "
+  for (i in 1:length(filters)) {
+    if (i == 1) {
+      query_cmd <- paste0(query_cmd,mart," WHERE ",filters[i]," = '",value[i],"' ")
+    } else {
+      query_cmd <- paste0(query_cmd,cond," ",filters[i]," = '",value[i],"' ")
+    }
+  }
+  res <- DBI::dbSendQuery(database, query_cmd)
+  attributes <- DBI::dbFetch(res)
+  attributes
+}
 
 
